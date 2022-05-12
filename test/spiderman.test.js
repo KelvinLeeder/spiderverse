02ff9fad8e766df8641868015ff4bcc89e0a02a7
KelvinLeeder <kleeder77@outlook.com>
2022-05-11 22:13:53 -0500

    Modificacion de archivo test para prueba de spiderman de andrew garfield

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,16 @@
-describe("Test suite Dummy Description", () => {
-    test('Case 1 dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18);
+describe("Unit Test for spiderman class", () => {
+    test('Creat an Spiderman object', () => {
+
+        //Aqui escribimos el codigo que queremos usar
+        //Quiero poder instanciar un objeto spiderman con esta informacion
+
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+        //Validamos que este codigo funcione de la forma esperada
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfied")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony")
     });
 })
